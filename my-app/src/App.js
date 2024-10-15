7d3ebda143a9df0ee94f1ece7a3b933c550b29f2
unknown <eric.lecouffe@decathlon.com>
2024-10-15 12:11:20 +0200

    add jeu dino

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChromeDino from 'react-chrome-dino';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Chrome Dino Game</h1>
+      <ChromeDino />
+    </div>
+  );
+}
+
+export default App;
